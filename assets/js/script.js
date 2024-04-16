846567080352d62c85a88695be9ec5173dcfb097
andgeral <geraldinebecerra93@gmail.com>
2024-04-16 00:03:10 -0400

    se agrega funcionalidad  a las imagenes cuando se les hace click se agrega imagen al carro y se pone opaca

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log("hola");
+// array vacio para ir agregando las imagenes
+let carrito = []
+
+//llamamos a la imagen
+const imagenes = document.querySelectorAll('.cambiar_color')
+
+//llamamos al modal body
+const carritoImg = document.querySelector('.modal-body')
+
+//hacemos un foreach a las imagenes para cuando se le haga click a la imagen en especifico se ponga opaca
+imagenes.forEach((img) => {
+  img.addEventListener('click', () => {
+    img.style.opacity = '0.5'
+    const imagenClonada = img.cloneNode(true)
+    carrito.push(imagenClonada)
+
+    // Mostrar la imagen en el carrito,creamos el elemento img luego asignmos la imagenClonada
+    //finalmente al carritoImg le agregamos la imagen
+    const imagenEnCarrito = document.createElement('img')
+    imagenEnCarrito.src = imagenClonada.src
+    imagenEnCarrito.style.width = '100px'
+    carritoImg.appendChild(imagenEnCarrito)
+  })
+})
